af0857f7f80c8c6d341db5fee8e3c762b21c9d69
zbohannan <zbohannan@gmail.com>
2017-01-29 20:49:26 -0600

    Update DESeq code

--- TCGA-AML.r
+++ TCGA-AML.r
@@ -1,16 +1,28 @@
+
+# This has turned out to be nightmare fuel after reboot.
+# R had problems finding Rcpp, so had to reinstall with dependencies
+# Also had problems with limma from Bioconductor, but hopefully that's sorted
+
 library(TCGAbiolinks)
-AMLquery <- GDCquery(project = "TARGET-AML", 
+library(DESeq2)
+
+# Download AML RNAseq data
+aml.rnaseq.query <- GDCquery(project = "TCGA-LAML", 
                      data.category = "Transcriptome Profiling", 
                      data.type = "Gene Expression Quantification", 
                      workflow.type = "HTSeq - Counts")
-GDCdownload(AMLquery)
-aml.rnaseq <- GDCprepare(query = AMLquery, 
-                         save = True, 
-                         save.filename = "amlRnaseq.rda", 
+GDCdownload(aml.rnaseq.query, method = "client")
+aml.rnaseq <- GDCprepare(query = aml.rnaseq.query, 
+                         save = TRUE, 
+                         save.filename = "tcga-amlRnaseq.rda", 
                          summarizedExperiment = TRUE)
-# need to figure out how to open clinical data.
-# currently getting errors using both GDCquery_clinic
-# and GDCprepare_clinic
 
-# Error in doc_parse_file(con, encoding = encoding, as_html = as_html, options = options) : 
-# basic_string::resize
+# Download clinical data
+aml.clin.query <- GDCquery(project = "TCGA-LAML",
+                      data.category = "Clinical")
+GDCdownload(aml.clin.query, method="client")
+aml.patient <- GDCprepare_clinic(aml.clin.query, clinical.info = "patient")
+
+# Run DESeq2 on AML count data
+aml.rnaseq.DE <- DESeqDataSet(aml.rnaseq, design = ~ patient)
+aml.rnaseq.DE <- DESeq(aml.rnaseq.DE)
